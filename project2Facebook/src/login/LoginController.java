563341c54b1bf046bb0a064663d8ed82641b75e5
Tim <Tim@Tim-laptop>
2017-11-28 23:37:54 -0800

    Connnected LoginController and OwnProfile[View/Model]
    Correct login now creates and opens OwnProfileView
    LoginView constructor now requires AccountModel parameter
    	Should it be in LoginModel instead?

--- project2Facebook/src/login/LoginController.java
+++ project2Facebook/src/login/LoginController.java
@@ -3,13 +3,19 @@ package login;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 
+import profile.AccountModel;
+import profile.OwnProfileView;
+import profile.OwnProfileController;
+
 public class LoginController {
 	private LoginView li_view;
 	private LoginModel li_model;
+	private AccountModel account;
 	
-	public LoginController(LoginView view, LoginModel model) {
+	public LoginController(LoginView view, LoginModel model, AccountModel account) {
 		this.li_view = view;
 		this.li_model = model;
+		this.account = account;
 		
 		view.addEnterListener(new LoginListener());
 		view.setVisible(true);
@@ -20,12 +26,8 @@ public class LoginController {
 			String username = li_view.getUsername();
 			String password = li_view.getPassword();
 			if (li_model.verify(username, password)) {
-				li_view.setVisible(false);
-				
-				// Show OwnProfileView
-				System.out.println("Successful login");
-				
 				li_view.dispose();
+				new OwnProfileController(new OwnProfileView(), account);
 			}
 			else {
 				li_view.showError("Incorrect login information");
--- project2Facebook/src/login/LoginTestDriver.java
+++ project2Facebook/src/login/LoginTestDriver.java
@@ -1,10 +1,34 @@
 package login;
 
+import java.awt.Image;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
+import profile.AccountModel;
+
 public class LoginTestDriver {
 	public static void main(String[] args) {
-		LoginModel	model	= new LoginModel("tlee70", "terriblePassword");
-		LoginView	view 	= new LoginView();
-		LoginController	controller = new LoginController(view, model);
+		try {
+			Image image = ImageIO.read(new File("C:/Users/Tim/Pictures/Pics/Symbol.png"));
+			//Image image = ImageIO.read(new File("//home/tlee70/git/cpe305-project2-facebook/project2Facebook/pics/blank.png"));
+			AccountModel myAcc = new AccountModel("Tim Lee", image);
+		
+			myAcc.addFriend(new AccountModel("Sam Haire", image));
+			myAcc.addFriend(new AccountModel("Steven Meissner", image));
+			myAcc.addFriend(new AccountModel("Taylor Wood", image));
+			myAcc.addFriend(new AccountModel("Yesenia Moreno", image));
+			myAcc.addFriend(new AccountModel("Caleb Barber", image));
+			myAcc.addFriend(new AccountModel("Dylan Kirkby", image));
+			
+			LoginModel	model	= new LoginModel("tlee70", "terriblePassword");
+			LoginView	view 	= new LoginView();
+			LoginController	controller = new LoginController(view, model, myAcc);
+		}
+		catch (IOException e) {
+			System.out.println(e.getStackTrace());
+		}
 		
 	}
 }
