5499d826fc983f1fcf13a6862342bcbfff49c309
Tim <Tim@Tim-laptop>
2017-12-04 15:33:08 -0800

    Changed password field from JPasswordField to JTextField
    Easier to read and compare results
    Security not actually a concern for this project

--- project2Facebook/src/login/LoginView.java
+++ project2Facebook/src/login/LoginView.java
@@ -6,7 +6,6 @@ import java.awt.event.ActionListener;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
-import javax.swing.JPasswordField;
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
@@ -14,7 +13,8 @@ import javax.swing.JOptionPane;
 public class LoginView extends JFrame{
 	
 	private JTextField usernameTf = new JTextField(15);
-	private JPasswordField passwordTf = new JPasswordField(20);
+	//private JPasswordField passwordTf = new JPasswordField(20);
+	private JTextField passwordTf = new JTextField(20);
 	private JButton enterBtn = new JButton("Enter");
 	
 	public LoginView () {
